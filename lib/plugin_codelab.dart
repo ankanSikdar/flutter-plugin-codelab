9d6b55e3d7bdd18a589c8a36d6663183917fd849
Ankan Sikdar <ankansikdar@gmail.com>
2021-10-19 21:47:56 +0530

    Designed the plugin api

--- lib/plugin_codelab.dart
+++ lib/plugin_codelab.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -9,5 +8,15 @@ class PluginCodelab {
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<int?> onKeyDown(int key) async {
+    final int? numNotesOn = await _channel.invokeMethod('onKeyDown', [key]);
+    return numNotesOn;
+  }
+
+  static Future<int?> onKeyUp(int key) async {
+    final int? numNotesOn = await _channel.invokeMethod('onKeyUp', [key]);
+    return numNotesOn;
   }
 }
